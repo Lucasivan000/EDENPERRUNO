220843a4893c3b4d96f876f97971b12f989bf71f
Lucas Cardozo <lucasivancardozo000@gmail.com>
2026-03-28 17:09:31 -0300

    guardar

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 
 
+const toggle = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
+
+toggle.addEventListener('click', () => {
+    navbar.classList.toggle('open');
+});
+
 
