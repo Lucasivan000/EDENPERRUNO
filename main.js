23778910b4cd8e3b244bae6751a94865a3c90b0b
Lucas Cardozo <lucasivancardozo000@gmail.com>
2026-03-28 22:51:25 -0300

    ja

--- main.js
+++ main.js
@@ -2,9 +2,18 @@
 
 const toggle = document.querySelector('.menu-toggle');
 const navbar = document.querySelector('.navbar');
+const menuLinks = document.querySelectorAll('.menu-links a'); // TODOS los links
 
+// Toggle menú hamburguesa
 toggle.addEventListener('click', () => {
     navbar.classList.toggle('open');
+});
+
+// Cerrar menú automáticamente al click en un link
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('open');
+    });
 });
 
 
